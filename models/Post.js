da8399db41b82cfd55f0dd07ed9784da196798c3
Michael Desanker <mdesanker@gmail.com>
2022-01-07 15:02:32 -0500

    Create post model

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+  published: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("Post", PostSchema);

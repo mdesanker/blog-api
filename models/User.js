d5135e217c2fc781237f1820e92474716e690732
Michael Desanker <mdesanker@gmail.com>
2022-01-07 15:10:08 -0500

    Create comment model

--- models/User.js
+++ models/User.js
@@ -2,16 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first: { type: String, required: true },
-  last: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 7 },
   date: { type: Date, default: Date.now() },
-});
-
-// Virtual for full name
-UserSchema.virtual("name").get(function () {
-  return `${this.first} ${this.last}`;
+  admin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("User", UserSchema);

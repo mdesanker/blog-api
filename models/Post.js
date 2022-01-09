5e408058753243071759f029513dc8dd171ffa56
Michael Desanker <mdesanker@gmail.com>
2022-01-09 09:18:07 -0500

    Add likes attribute to post model

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,7 @@ const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   date: { type: Date, default: Date.now },
   publish: { type: Boolean, default: false },
 });
